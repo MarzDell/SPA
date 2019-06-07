9b28ee63593bf6c6d2536ea9d1c4d39dd79592c6
Ubuntu <ubuntu@ip-172-31-24-181.ec2.internal>
2019-06-07 12:06:13 +0000

    Adding index.js to the html

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+const app = {
+    pages: [],
+    show: new Event('show'),
+    init:function (){
+       app.pages = document.querySelectorAll('.page');
+       app.pages.forEach((pg)=>{
+           pg.addEventListener('show', app.pageShown);
+       })
+       
+       document.querySelectorAll('.nav-link').forEach((link)=>{
+           link.addEventListener('click', app.nav);
+       })
+       history.replaceState({}, 'Home', '#home');
+       window.addEventListener('popstate', app.poppin);
+    },
+    
+    nav: function(ev){
+        ev.preventDefault();
+        let currentPage = ev.target.getAttribute('data-target');
+        document.querySelector('.active').classList.remove('active');
+        document.getElementById(currentPage).classList.add('active');
+        history.pushState({}, currentPage, `#${currentPage}`);
+        document.getElementById(currentPage).dispatchEvent(app.show);
+    },
+    pageShown: function(ev){
+        let h2 = ev.target.querySelector('h2');
+        h2.classList.add('medium')
+        setTimeout((h)=>{
+            h.classList.remove('medium');
+        }, 1000, h2);
+        
+    },
+    poppin: function(ev) {
+        let hash = location.hash.replace('#', '');
+        document.querySelector('.active').classList.remove('active');
+        document.getElementById(hash).classList.add('active');
+        document.getElementById(hash).dispatchEvent(app.show);
+    }
+    }
+
+document.addEventListener('DOMContentLoaded', app.init);

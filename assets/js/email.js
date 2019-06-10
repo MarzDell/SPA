7c2c3a6398c7702f7d9c4de9f0c4e2b03aa8f124
MarzDell <chodnickamarzena@yahoo.co.uk>
2019-06-10 16:34:29 +0000

    Fixing bugs

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,12 +1,17 @@
-var sendMail ={
-    'from_name': name.value,
-    'from_email': emailaddress.value,
-    'request': request.value
+function sendMail(contactForm) {
+    emailjs.send("gmail", "single_page_applocation", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        'option': contactForm.option.value,
+        "request": contactForm.request.value
+    })
+    .then(
+        function(response) {
+            console.log("Success!", response);
+        },
+        function(error) {
+            console.log("Failed....", error);
+        }
+    );
+    return false; 
 }
- 
-emailjs.send('gmail', 'single_page_applocation', sendMail)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
